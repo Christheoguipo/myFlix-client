66e4a6fec8433947b88e160a359fc08156d11414
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-10-14 02:10:34 +0200

    Import React to
    - main-view.js, movie-card, movie-view, index.js

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
+import React from 'react';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
