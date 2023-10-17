6322f62d1d2ebfcea25c8fd153198d9837e79eaa
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-10-17 23:49:45 +0200

    Update main-view.jsx
    - Import LoginView and SingupView.
    - Render Login and Signup view if user is not logged in.
    - Add Logout button.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,12 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { SignupView } from '../signup-view/signup-view';
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
+
+
 
   useEffect(() => {
-    fetch("https://retro-movie-vault-5ccf6999c998.herokuapp.com/movies")
+    if (!token) {
+      return;
+    }
+
+    fetch("https://retro-movie-vault-5ccf6999c998.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
       .then((response) => response.json())
       .then((data) => {
         const moviesFromApi = data.map((data) => {
@@ -32,9 +49,7 @@ export const MainView = () => {
 
         setMovies(moviesFromApi);
       });
-  }, []);
-
-  const [selectedMovie, setSelectedMovie] = useState(null);
+  }, [token]);
 
   if (selectedMovie) {
     return (
@@ -44,6 +59,19 @@ export const MainView = () => {
           setSelectedMovie(null);
         }}
       />
+    );
+  }
+
+  if (!user) {
+    return (
+      <>
+        <LoginView onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }} />
+        or
+        <SignupView />
+      </>
     );
   }
 
@@ -64,6 +92,11 @@ export const MainView = () => {
           />
         );
       })}
+      <button onClick={() => {
+        setUser(null);
+        setToken(null);
+        localStorage.clear();
+      }}>Logout</button>
     </div>
   );
 };
