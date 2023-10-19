1d43742ff2106e80eb84cbfcbac956adbfe6e458
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-10-20 00:32:29 +0200

    Update movie-card.jsx
    - Use react client side routing.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,18 +1,19 @@
 import PropTypes from "prop-types";
 import React from 'react';
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
     <Card className="h-100">
       <Card.Img variant="top" src={movie.Imageurl} />
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
         <Card.Text>{movie.Director.Name}</Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie._id)}`} >
+          <Button variant="link">Open</Button>
+        </Link>
       </Card.Body>
-      <Button variant="link" onClick={() => { onMovieClick(movie); }}>
-        Open
-      </Button>
     </Card>
   );
 };
@@ -28,6 +29,5 @@ MovieCard.propTypes = {
       Name: PropTypes.string.isRequired
     }),
     Imageurl: PropTypes.string.isRequired
-  }),
-  onMovieClick: PropTypes.func.isRequired
+  })
 }
