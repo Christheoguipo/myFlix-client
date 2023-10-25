74a95310e21116693d11d6468f39e0d5a5e45138
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-10-26 00:05:00 +0200

    Update navigation-bar.jsx
    - Add Logout to the navigation bar.

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -27,7 +27,12 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                 <Nav.Link as={Link} to="/">
                   Home
                 </Nav.Link>
-                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+                <Nav.Link as={Link} to={`/users/${user.Username}`}>
+                  Profile
+                </Nav.Link>
+                <Nav.Link onClick={onLoggedOut}>
+                  Logout
+                </Nav.Link>
               </>
             )}
           </Nav>
