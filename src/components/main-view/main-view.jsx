1948ea604ebe3a4b4dfc930ed3e6bd531a3c321f
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-10-26 00:03:37 +0200

    Update main-view.jsx
    - Add feature to add/remove movies from user's favorite movie list.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,6 +4,7 @@ import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import { NavigationBar } from "../navigation-bar/navigation-bar";
+import { ProfileView } from "../profile-view/profile-view";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -13,12 +14,49 @@ export const MainView = () => {
   const storedToken = localStorage.getItem("token");
 
   const [movies, setMovies] = useState([]);
-  // const [selectedMovie, setSelectedMovie] = useState(null);
+  const [users, setUsers] = useState([]);
 
   const [user, setUser] = useState(storedUser ? storedUser : null);
   const [token, setToken] = useState(storedToken ? storedToken : null);
 
+  const [favoriteMovies, setFavoriteMovies] = useState(user ? movies.filter((m) => user.FavoriteMovies.includes(m._id)) : []);
 
+  // let favoriteMovies = movies.filter(m => user ? user.FavoriteMovies.includes(m._id) : []);
+
+  const handleOnAddToFavorites = (movieId) => {
+    const updatedUser = user;
+
+    const updatedUsers = users;
+    const userIndex = updatedUsers.findIndex((u) => u._id === user._id);
+
+    updatedUser.FavoriteMovies.push(movieId);
+    updatedUsers[userIndex] = updatedUser;
+
+    setUser(updatedUser);
+    setUsers(updatedUsers);
+
+    if (user) {
+      setFavoriteMovies(movies.filter((m) => user.FavoriteMovies.includes(m._id)));
+    }
+  }
+
+  const handleOnRemoveFromFavorites = (movieId) => {
+    const updatedUser = user;
+
+    const updatedUsers = users;
+    const userIndex = updatedUsers.findIndex((u) => u._id === user._id);
+
+    updatedUser.FavoriteMovies = updatedUser.FavoriteMovies.filter(favMovieId => movieId != favMovieId);
+    updatedUsers[userIndex] = updatedUser;
+
+    setUser(updatedUser);
+    setUsers(updatedUsers);
+
+    if (user) {
+      setFavoriteMovies(movies.filter((m) => user.FavoriteMovies.includes(m._id)));
+    }
+
+  }
 
   useEffect(() => {
     if (!token) {
@@ -28,7 +66,15 @@ export const MainView = () => {
     fetch("https://retro-movie-vault-5ccf6999c998.herokuapp.com/movies", {
       headers: { Authorization: `Bearer ${token}` }
     })
-      .then((response) => response.json())
+      .then((response) => {
+
+        if (!response.ok) {
+          console.log(response);
+          throw new Error(`HTTP Error: ${response.status}`);
+        }
+
+        return response.json();
+      })
       .then((data) => {
         const moviesFromApi = data.map((data) => {
           return {
@@ -50,8 +96,44 @@ export const MainView = () => {
             Featured: data.Featured
           };
         });
+
         setMovies(moviesFromApi);
+        setFavoriteMovies(movies.filter(m => user ? user.FavoriteMovies.includes(m._id) : []));
+
+      })
+      .catch((e) => {
+        console.log(e);
+        alert("Error occurred while getting all movies.");
       });
+
+    fetch("https://retro-movie-vault-5ccf6999c998.herokuapp.com/users", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP Error: ${response.status}`);
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+        const usersFromApi = data.map((data) => {
+          return {
+            _id: data._id,
+            Username: data.Username,
+            Password: data.Password,
+            Email: data.Email,
+            Birthday: data.Birthday,
+            FavoriteMovies: data.FavoriteMovies
+          };
+        });
+        setUsers(usersFromApi);
+      })
+      .catch((e) => {
+        console.log(e);
+        alert("Error occurred while getting all users.");
+      });
+
   }, [token]);
 
   return (
@@ -66,7 +148,47 @@ export const MainView = () => {
       />
 
       <Row className="justify-content-md-center">
+
         <Routes>
+
+          <Route
+            path="/users/:usernameParam"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : users.length === 0 ? (
+                  <Col>No users found!</Col>
+                ) : (
+                  <Col md={8}>
+                    <ProfileView users={users} token={token}
+
+                      favoriteMovies={favoriteMovies}
+
+                      onProfileUpdate={(user) => {
+                        setUser(user);
+                      }}
+
+                      onProfileDeregister={() => {
+                        setUser(null);
+                        setToken(null);
+                        localStorage.clear();
+                      }}
+
+                      onAddToFavorites={(movieId) => {
+                        handleOnAddToFavorites(movieId);
+                      }}
+
+                      onRemoveFromFavorites={(movieId) => {
+                        handleOnRemoveFromFavorites(movieId);
+                      }}
+                    />
+                  </Col>
+                )}
+              </>
+            }
+          />
+
           <Route
             path="/signup"
             element={
@@ -129,7 +251,16 @@ export const MainView = () => {
                   <>
                     {movies.map((movie) => (
                       <Col className="mb-5" key={movie._id} md={3}>
-                        <MovieCard movie={movie} />
+                        <MovieCard movie={movie} user={user} token={token}
+
+                          onAddToFavorites={(movieId) => {
+                            handleOnAddToFavorites(movieId);
+                          }}
+
+                          onRemoveFromFavorites={(movieId) => {
+                            handleOnRemoveFromFavorites(movieId);
+                          }}
+                        />
                       </Col>
                     ))}
                   </>
