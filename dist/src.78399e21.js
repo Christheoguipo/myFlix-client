04a93749b3155a7b83af0b7c71005d09ea1a456b
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-10-28 22:45:15 +0200

    Parcel output

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -55539,7 +55539,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51294" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50829" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
