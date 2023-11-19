b6fbef7a91c7ea96d1f3f477ad9a93a0012a7e49
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-11-19 04:09:37 +0100

    Update profile-view.jsx
    Logs out user after profile update.
    Add modal for confirmation.
    Changed javscript alerts to React alerts.
    Adjust responsiveness.

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import Button from "react-bootstrap/Button";
 import { MovieCard } from "../movie-card/movie-card";
 import Row from "react-bootstrap/Row";
@@ -7,13 +7,23 @@ import Col from "react-bootstrap/Col";
 import { useDispatch } from "react-redux";
 import { setUser, setToken } from "../../redux/reducers/user";
 import Form from "react-bootstrap/Form";
+import { Alert, Modal } from "react-bootstrap";
 
-export const ProfileView = ({ users, token, movies }) => {
+export const ProfileView = ({ user, token, movies }) => {
+
+  const navigate = useNavigate();
+
+  const { usernameParam } = useParams();
+  const isUser = user.Username === usernameParam;
+
+  if (!isUser) {
+    navigate("/");
+  }
 
   const dispatch = useDispatch();
 
-  const { usernameParam } = useParams();
-  const user = users.find((user) => user.Username === usernameParam);
+  // Commented out to get the user from the mainview instead
+  // const user = users.find((user) => user.Username === usernameParam);
 
   const favoriteMovies = movies.filter((movie) => user.FavoriteMovies.includes(movie._id));
   const formattedDate = new Date(user.Birthday).toISOString().substring(0, 10);
@@ -23,11 +33,33 @@ export const ProfileView = ({ users, token, movies }) => {
   const [email, setEmail] = useState(user.Email);
   const [birthday, setBirthday] = useState(formattedDate);
 
-  const [updateUser, setUpdateUser] = useState(user);
+
+  const [updateModalShow, setUpdateModalShow] = useState(false);
+  const [deleteModalShow, setDeleteModalShow] = useState(false);
+
+  const handleUpdateModalShow = () => setUpdateModalShow(true);
+  const handleUpdateModalClose = () => setUpdateModalShow(false)
+  const handleDeleteModalShow = () => setDeleteModalShow(true);
+  const handleDeleteModalClose = () => setDeleteModalShow(false);
+
+  const [alertVariant, setAlertVariant] = useState("success");
+  const [alertShow, setAlertShow] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const alertProperties = (variant, show, message) => {
+    setAlertVariant(variant);
+    setAlertShow(show);
+    setAlertMessage(message);
+  }
 
   const handleProfileUpdateSubmit = (event) => {
     event.preventDefault();
 
+    handleUpdateModalShow();
+
+  };
+
+  const handleConfirmUpdate = () => {
     const data = {
       Username: username,
       Password: password,
@@ -53,85 +85,82 @@ export const ProfileView = ({ users, token, movies }) => {
       .then((data) => {
 
         if (data) {
-          console.log("User update response: ", data);
 
+          alertProperties("success", true, "User successfully updated.")
 
+          dispatch(setUser(null));
+          dispatch(setToken(null));
           localStorage.clear();
-          window.location.reload();
-
-          // dispatch(setUser(data));
-          // localStorage.setItem("user", JSON.stringify(data));
-
-          // //update the users after a user has updated
-          // const userIndex = users.findIndex((user) => user._id === data._id);
-          // const updatedUsers = [...users];
-          // updatedUsers[userIndex] = data;
-          // dispatch(setUsers(updatedUsers));
-
-          // alert("User successfully updated!");
+          handleUpdateModalClose();
 
         } else {
-          alert("No such user.");
+          alertProperties("danger", true, "No such user.")
         }
       })
       .catch((e) => {
         console.log(e);
-        alert("Error occurred during user update.");
+        alertProperties("danger", true, "Error occurred during user update.")
       });
-
-  };
+  }
 
   const handleDeregisterSubmit = (event) => {
     event.preventDefault();
 
-    if (confirm("This will remove your account permanently. Are you sure you want to deregister?")) {
+    handleDeleteModalShow();
+  }
 
-      fetch(`https://retro-movie-vault-5ccf6999c998.herokuapp.com/users/${user.Username}`, {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${token}`
+  const handleConfirmDelete = () => {
+
+    fetch(`https://retro-movie-vault-5ccf6999c998.herokuapp.com/users/${user.Username}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP Error: ${response.status}`);
+        }
+
+        return response;
+      })
+      .then((data) => {
+
+        if (data) {
+
+          alertProperties("success", true, "User successfully deregistered.")
+
+          dispatch(setUser(null));
+          dispatch(setToken(null));
+          localStorage.clear();
+
+          handleDeleteModalClose();
+
+        } else {
+          alertProperties("danger", true, "No such user.")
         }
       })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP Error: ${response.status}`);
-          }
-
-          return response;
-        })
-        .then((data) => {
-          console.log("User Deregister response: ", data);
-
-          if (data) {
-
-            alert("User successfully deregistered!");
-
-            dispatch(setUser(null));
-            dispatch(setToken(null));
-            localStorage.clear();
-          } else {
-            alert("No such user.");
-          }
-        })
-        .catch((e) => {
-          console.log(e);
-          alert("Error occurred during user deregister.");
-        });
-    }
+      .catch((e) => {
+        console.log(e);
+        alertProperties("danger", true, "Error occurred during user deregister.")
+      });
   }
 
   return (
     <>
+      <Alert variant={alertVariant} show={alertShow} onClose={() => setAlertShow(false)} dismissible>
+        {alertMessage}
+      </Alert>
+
       <Row>
-        <Col md={4}>
+        <Col xl={6}>
           <h3>Account Information</h3>
-          <Form onSubmit={handleProfileUpdateSubmit}>
+          <Form id="formUpdate" onSubmit={handleProfileUpdateSubmit}>
             <Form.Group controlId="formProfileUsername">
-              {/* <Form.Label>Username</Form.Label> */}
+              <Form.Label>Username</Form.Label>
               <Form.Control
                 className="mb-2"
                 type="text"
-                placeholder="Username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 required
@@ -140,11 +169,10 @@ export const ProfileView = ({ users, token, movies }) => {
             </Form.Group>
 
             <Form.Group controlId="formProfilePassword">
-              {/* <Form.Label>Password</Form.Label> */}
+              <Form.Label>Password</Form.Label>
               <Form.Control
                 className="mb-2"
                 type="password"
-                placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
@@ -152,11 +180,10 @@ export const ProfileView = ({ users, token, movies }) => {
             </Form.Group>
 
             <Form.Group controlId="formProfileEmail">
-              {/* <Form.Label>Email</Form.Label> */}
+              <Form.Label>Email</Form.Label>
               <Form.Control
                 className="mb-2"
                 type="email"
-                placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
@@ -164,11 +191,10 @@ export const ProfileView = ({ users, token, movies }) => {
             </Form.Group>
 
             <Form.Group controlId="formProfileBirthday">
-              {/* <Form.Label>Birthday</Form.Label> */}
+              <Form.Label>Birthday</Form.Label>
               <Form.Control
                 className="mb-2"
                 type="date"
-                placeholder="Birthday"
                 value={birthday}
                 onChange={(e) => setBirthday(e.target.value)}
                 required
@@ -177,7 +203,8 @@ export const ProfileView = ({ users, token, movies }) => {
             <div className="d-flex justify-content-between">
 
               <Button className="mt-2" variant="primary" type="submit">Update Profile</Button>
-              <Button form="formDeregister" className="mt-2" variant="danger" type="submit">Delete Account</Button>
+              <Button form="formDeregister" className="mt-2" variant="danger" type="submit">Delete Profile</Button>
+
             </div>
 
           </Form>
@@ -187,23 +214,56 @@ export const ProfileView = ({ users, token, movies }) => {
         </Col>
       </Row>
 
+      <Modal show={updateModalShow} onHide={handleUpdateModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update Account Information</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to update your Account information?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleUpdateModalClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleConfirmUpdate} >
+            Update Profile
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={deleteModalShow} onHide={handleDeleteModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Profile</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to delete your Profile?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleDeleteModalClose}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={handleConfirmDelete} >
+            Delete Profile
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <hr />
 
       <h3>Favorite Movies</h3>
-      {favoriteMovies.length === 0 ? (
-        <div>
-          Your list of favorite movies is empty!
-        </div>
-      ) : (
-        <Row>
-          {favoriteMovies.map((movie) => (
-            <Col className="mb-5" key={movie._id} md={2}>
-              <MovieCard movie={movie} />
-            </Col>
-          ))}
-        </Row>
-      )}
-
+      {
+        favoriteMovies.length === 0 ? (
+          <div></div>
+        ) : (
+          <Row>
+            {favoriteMovies.map((movie) => (
+              <Col className="mb-5" key={movie._id} xxl={2} xl={3} lg={4} md={4} sm={6} xs={12}>
+                <MovieCard movie={movie} />
+              </Col>
+            ))}
+          </Row>
+        )
+      }
     </>
   );
 };
